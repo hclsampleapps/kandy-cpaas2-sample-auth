393c6f40174662fd57e40fc516dc5a915ae2216f
Abhishek Kumar <akbittu@gmail.com>
2019-04-12 05:32:35 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # kandy-sample-auth
+
+### Auth app
+
+This app will generate auth token for accessing further APIs.
+
+#### Steps 
+
+1. Create an account on **AT&T** portal via [Register now for a free account](https://apimarket.att.com/signup).
+2. After signup get the following 
+   - *clientId* 
+   - *emailId* 
+   - *password*
+3. If you are an existing user, please [Log in to AT&T API Marketplace](https://apimarket.att.com/login).
+4. Download *kandy.js* from [Developer documentation - SDKs](https://apimarket.att.com/developer/sdks/javascript)
+5. Open the index.html in the browser.
+6. After successful login you will get an *accessToken* that you can use to explore the other APIs.
---
+++ src/js/app.js
@@ -0,0 +1,226 @@
+/**
+ * CPaaS Authentication Demo
+ */
+var changeView;
+var showhideView;
+var serverBase;
+var mHostUrl;
+var client;
+const tokenAPI = '/cpaas/auth/v1/token'
+
+whenReady(function() {
+    changeView = new ChangeView();
+    changeView.showPasswordGrant();
+});
+
+class ChangeView {
+    constructor() {
+        this.accountPasswordGrantView = document.getElementById('passwordID');
+        this.accountClientCredentialsView = document.getElementById('clientCredID');
+
+        this.accountPasswordGrantradio = document.getElementById('passwordGrant');
+        this.accountPasswordGrantradio.addEventListener('click', (evt) => this.showPasswordGrant(evt));
+
+        this.accountClientCredentialsradio = document.getElementById('clientCred');
+        this.accountClientCredentialsradio.addEventListener('click', (evt) => this.showClientCredentials(evt));
+    }
+
+    showPasswordGrant() {
+        Effect.hide(this.accountClientCredentialsView);
+        Effect.show(this.accountPasswordGrantView);
+    }
+
+    showClientCredentials() {
+        Effect.show(this.accountClientCredentialsView);
+        Effect.hide(this.accountPasswordGrantView);
+    }
+}
+
+function initClient() {
+    let mServerUrl = document.getElementById("serverUrl").value;
+    mHostUrl = new URL(mServerUrl).host;
+    console.log(mHostUrl);
+    client = Kandy.create({
+        subscription: {
+            expires: 3600
+        },
+        // Required: Server connection configs.
+        authentication: {
+            server: {
+                base: mHostUrl
+            },
+            clientCorrelator: 'sampleCorrelator'
+        }
+    })
+}
+
+/**
+ * Creates a form body from an dictionary
+ */
+function createFormBody(paramsObject) {
+    const keyValuePairs = Object.entries(paramsObject).map(
+        ([key, value]) => encodeURIComponent(key) + '=' + encodeURIComponent(value)
+    )
+    return keyValuePairs.join('&')
+}
+
+/**
+ * Gets the tokens necessary for authentication to CPaaS
+ */
+async function getTokensByPasswordGrant({
+    clientId,
+    username,
+    password
+}) {
+    const cpaasAuthUrl = constructServerUrl();
+    const formBody = createFormBody({
+        client_id: clientId,
+        username,
+        password,
+        grant_type: 'password',
+        scope: 'openid'
+    })
+    // POST a request to create a new authentication access token.
+    const fetchResult = await fetch(cpaasAuthUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formBody
+    })
+    // Parse the result of the fetch as a JSON format.
+    const data = await fetchResult.json()
+    return {
+        accessToken: data.access_token,
+        idToken: data.id_token
+    }
+}
+
+async function loginByPasswordGrant() {
+    initClient();
+    const clientId = document.getElementById('clientId').value
+    const userEmail = document.getElementById('userEmail').value
+    const password = document.getElementById('password').value
+    try {
+        const tokens = await getTokensByPasswordGrant({
+            clientId,
+            username: userEmail,
+            password
+        })
+
+        log('Successfully logged in as ' + userEmail)
+        if (tokens.accessToken === undefined)
+            document.getElementById('accessToken').value = "Please enter the valid credentials!"
+        else
+            document.getElementById('accessToken').value = tokens.accessToken
+
+        client.setTokens(tokens)
+
+    } catch (error) {
+        log('Error: Failed to get authentication tokens. Error: ' + error)
+    }
+}
+
+async function getTokensByClientCredGrant({
+    client_id,
+    client_secret
+}) {
+
+    const cpaasAuthUrl = constructServerUrl();
+    const formBody = createFormBody({
+        client_id,
+        client_secret,
+        grant_type: 'client_credentials',
+        scope: 'openid regular_call'
+    })
+
+    // POST a request to create a new authentication access token.
+    const fetchResult = await fetch(cpaasAuthUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formBody
+    })
+    // Parse the result of the fetch as a JSON format.
+    const data = await fetchResult.json();
+
+    return {
+        accessToken: data.access_token,
+        idToken: data.id_token
+    }
+}
+
+async function loginByClientCred() {
+    initClient();
+    const privateKey = document.getElementById('privateKey').value
+    const privateSecret = document.getElementById('privateSecret').value
+
+    try {
+        const tokens = await getTokensByClientCredGrant({
+            client_id: privateKey,
+            client_secret: privateSecret
+        })
+        client.setTokens(tokens)
+        log('Successfully logged in with project User ' + privateKey)
+
+        if (tokens.accessToken === undefined)
+            document.getElementById('accessToken').value = "Please enter the valid credentials!"
+        else
+            document.getElementById('accessToken').value = tokens.accessToken
+    } catch (error) {
+        log('Error: Failed to get authentication tokens. Error: ' + error)
+    }
+}
+
+function constructServerUrl() {
+    let cpaasUrl;
+    let enteredBaseUrl = document.getElementById("serverUrl").value
+    if (enteredBaseUrl.trim() !== "") {
+        serverBase = enteredBaseUrl.trim()
+    }
+    cpaasUrl = serverBase + tokenAPI
+    return cpaasUrl;
+}
+
+/**
+ * Gets the tokens necessary for authentication to CPaaS
+ */
+async function getTokens({ clientId, username, password }) {
+    const formBody = createFormBody({
+        client_id: clientId,
+        username,
+        password,
+        grant_type: 'password',
+        scope: 'openid'
+    })
+
+    // POST a request to create a new authentication access token.
+    const cpaasAuthUrl = 'https://nvs-cpaas-oauth.kandy.io/cpaas/auth/v1/token'
+    const fetchResult = await fetch(cpaasAuthUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formBody
+    })
+
+    // Parse the result of the fetch as a JSON format.
+    const data = await fetchResult.json()
+
+    return { accessToken: data.access_token, idToken: data.id_token }
+}
+
+function setTokens() {
+    const accessToken = document.getElementById('accessToken').value
+    const idToken = document.getElementById('idToken').value
+
+    client.setTokens({ accessToken, idToken })
+    log('Successfully set tokens')
+}
+
+// Utility function for appending messages to the message div.
+function log(message) {
+    console.log(message);
+    document.getElementById('terminal').innerHTML += '<p>' + message + '</p>';
+}
